0966213d08ddbc217e5e5e7e8d49e8fc3c5d9d69
dkilav <vlad5222vd@gmail.com>
2024-12-15 00:23:22 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container max-w-5xl">
+  <div>
+    <div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon1">@</span>
+  <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+</div>
+
+<div class="input-group mb-3">
+  <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
+  <span class="input-group-text" id="basic-addon2">@example.com</span>
+</div>
+
+<div class="mb-3">
+  <label for="basic-url" class="form-label">Your vanity URL</label>
+  <div class="input-group">
+    <span class="input-group-text" id="basic-addon3">https://example.com/users/</span>
+    <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3 basic-addon4">
+  </div>
+  <div class="form-text" id="basic-addon4">Example help text goes outside the input group.</div>
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text">$</span>
+  <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+  <span class="input-group-text">.00</span>
+</div>
+
+<div class="input-group mb-3">
+  <input type="text" class="form-control" placeholder="Username" aria-label="Username">
+  <span class="input-group-text">@</span>
+  <input type="text" class="form-control" placeholder="Server" aria-label="Server">
+</div>
+
+<div class="input-group">
+  <span class="input-group-text">With textarea</span>
+  <textarea class="form-control" aria-label="With textarea"></textarea>
+</div>
+  </div><br>
+  <h1 class="mt-2 bg-red-950 pt-4 text-3xl font-bold italic text-blue-600">
+    Lorem ipsum dolor sit amet.
+  </h1>
+  <h2 class="mt-2 bg-red-900 pt-4 text-3xl font-bold italic text-green-600">
+    Lorem ipsum dolor sit amet.
+  </h2><br>
+<div>
+  <button type="button" class="btn btn-danger">Danger</button>
+</div>
+</div>
+</template>
+
+<style></style>
